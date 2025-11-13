b921e5cff9946f24389a2d1aa624867888a569b9
marcospaulonerd-boop <marcospaulonerd@gmail.com>
2025-11-13 22:55:47 +0000

    alterando titulo do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
     <>
       <HeaderContainer>
         <HeaderContent>
-          <Logo href="/">ADS Animes</Logo>
+          <Logo href="/">AnimeFlix Uninassau</Logo>
 
           <Nav>
             <NavLink href="/">Home</NavLink>
